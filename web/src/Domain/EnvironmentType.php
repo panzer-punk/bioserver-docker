d008549f8839c78c6834ec39aed934ca0a52941a
Panzer Punk <gsuper36@gmail.com>
2026-03-06 20:55:20 +0300

    changed volume handling for biomysql, added staged builds for fpm

---
+++ web/src/Domain/EnvironmentType.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain;
+
+use TypeError;
+use ValueError;
+
+enum EnvironmentType: string
+{
+    case Production = "production";
+    case Development = "development";
+
+    /**
+     * @throws ValueError
+     * @throws TypeError
+     * 
+     * @return boolean
+     */
+    public static function isProduction(): bool
+    {
+        return self::Production === self::get();
+    }
+
+    /**
+     * @throws ValueError
+     * @throws TypeError
+     * 
+     * @return boolean
+     */
+    public static function isDevelopment(): bool
+    {
+        return ! self::isProduction();
+    }
+
+    /**
+     * @throws ValueError
+     * @throws TypeError
+     * 
+     * @return self
+     */
+    public static function get(): self
+    {
+        return self::from((string) $_ENV["APP_ENV"]);
+    }
+}
